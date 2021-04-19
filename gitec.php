cd536782fc2a6b3ff7f8be7a8db1392c7950cdf1
PavelMir <mironpavel123@gmail.com>
2021-04-20 00:57:58 +0300

    first second branch commit

--- gitec.php
+++ gitec.php
@@ -2,7 +2,28 @@
 1. Пишем первый код - индекс - комитт
 commit 8cdb19cf833ea98491ff7a7cc7d69c4059de1d84 (HEAD -> master)
 2.Меняем код - индекс - коммит
+commit 5038f1cecd030943f31b7bb299f5029a3d8fa957 (HEAD -> master)
+Author: PavelMir <mironpavel123@gmail.com>
+Date:   Tue Apr 20 00:27:44 2021 +0300
 
+    second master commit
 
+commit 8cdb19cf833ea98491ff7a7cc7d69c4059de1d84
+Author: PavelMir <mironpavel123@gmail.com>
+Date:   Tue Apr 20 00:25:48 2021 +0300
+3.Создаем новую ветку  git checkout -b second_branch
+
+commit 5038f1cecd030943f31b7bb299f5029a3d8fa957 (HEAD -> second_branch, master)
+Author: PavelMir <mironpavel123@gmail.com>
+Date:   Tue Apr 20 00:27:44 2021 +0300
+
+    second master commit
+
+commit 8cdb19cf833ea98491ff7a7cc7d69c4059de1d84
+Author: PavelMir <mironpavel123@gmail.com>
+Date:   Tue Apr 20 00:25:48 2021 +0300
+
+    first master commit
+4.Меняем код - индекс - коммит
 
 ?>
