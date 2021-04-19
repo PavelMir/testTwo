b1c648a3821f734d644f09f297d6bc97eacd5e15
PavelMir <mironpavel123@gmail.com>
2021-04-20 00:59:30 +0300

    second second branch commit

--- gitec.php
+++ gitec.php
@@ -25,5 +25,22 @@ Date:   Tue Apr 20 00:25:48 2021 +0300
 
     first master commit
 4.Меняем код - индекс - коммит
+commit cd536782fc2a6b3ff7f8be7a8db1392c7950cdf1 (HEAD -> second_branch)
+Author: PavelMir <mironpavel123@gmail.com>
+Date:   Tue Apr 20 00:57:58 2021 +0300
 
+    first second branch commit
+
+commit 5038f1cecd030943f31b7bb299f5029a3d8fa957 (master)
+Author: PavelMir <mironpavel123@gmail.com>
+Date:   Tue Apr 20 00:27:44 2021 +0300
+
+    second master commit
+
+commit 8cdb19cf833ea98491ff7a7cc7d69c4059de1d84
+Author: PavelMir <mironpavel123@gmail.com>
+Date:   Tue Apr 20 00:25:48 2021 +0300
+
+    first master commit
+5.Меняем код - индекс - коммит
 ?>
